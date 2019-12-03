aa62326db7d89fdefd24e45adef7eb99b246e72e
Orlando Soto <oj.5070@gmail.com>
2019-12-03 02:58:35 -0500

    update

--- run_BSA.cxx
+++ run_BSA.cxx
@@ -1,8 +1,14 @@
 #include "BSA_survey_cls.h"
 
 
-int main(){
-  BSA_survey_cls t("../JLAB_DATA/data/RGA/mix/pruned_evData_skim4_5204_pippim_*.root");
+int main(int argc, char *argv[]){
+
+  if (argc!=2){
+    std::cout<<"You need to supply a file or a regexp"<<std::endl;
+    return 1;
+  }
+  
+  BSA_survey_cls t(argv[1]);
   t.Loop();
   return 0;
 }
